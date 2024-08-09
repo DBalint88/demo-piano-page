f5a2751f35531ff08f28bb3dbacdba5df5bc2f0f
DBalint88 <dbalint88@gmail.com>
2024-08-09 13:28:03 -0400

    Build basic userProfile and songData structure.  Input sample data.

---
+++ scripts/songData.js
@@ -0,0 +1,246 @@
+export const songData = [
+    {
+        level: 7,
+        sequence: 1,
+        title: "'Round the Mountain",
+        image: "images/07-shell-be-comin-round-the-mountain.pdf",
+        youtube: "https://youtu.be/T0kG5GiVvqQ"
+    },
+    {
+        level: 7,
+        sequence: 2,
+        title: "In An English Country Garden",
+        image: "images/07-english-country-garden.pdf",
+        youtube: "https://youtu.be/E_ETiKC96uk"
+    },
+    {
+        level: 7,
+        sequence: 3,
+        title: "Shoo Fly",
+        image: "images/07-shoo-fly.pdf",
+        youtube: "https://youtu.be/2Gh9HMHakio"
+    },
+    {
+        level: 7,
+        sequence: 4,
+        title: "Molly Malone",
+        image: "images/07-molly-malone.pdf",
+        youtube: "https://youtu.be/8DZRbUwHnYc"
+    },
+    {
+        level: 7,
+        sequence: 5,
+        title: "Korobushka",
+        image: "images/07-korobushka.pdf",
+        youtube: "https://youtu.be/t0hqlf3IxcU"
+    },
+    {
+        level: 7,
+        sequence: 6,
+        title: "Eine Kleine Nachtmusik",
+        image: "images/07-eine-kleine.pdf",
+        youtube: "https://youtu.be/Jw7ub1ZP0Zs"
+    },
+    {
+        level: 7,
+        sequence: 7,
+        title: "The Ants Go Marching",
+        image: "images/07-the-ants-go-marching.pdf",
+        youtube: "https://youtu.be/LtB_8rBhZk0"
+    },
+    {
+        level: 7,
+        sequence: 8,
+        title: "Casey Jones",
+        image: "images/07-casey-jones.pdf",
+        youtube: "https://youtu.be/5JOpR2wZeNQ"
+    },
+    {
+        level: 7,
+        sequence: 9,
+        title: "Home On the Range",
+        image: "images/07-home-on-the-range.pdf",
+        youtube: "https://youtu.be/Ku1LeHsDVVA"
+    },
+    {
+        level: 7,
+        sequence: 10,
+        title: "Wild Mountain Thyme",
+        image: "images/07-wild-mountain-thyme.pdf",
+        youtube: "https://youtu.be/019KWL55QkM"
+    },
+    {
+        level: 7,
+        sequence: 11,
+        title: "Troika",
+        image: "images/07-troika.pdf",
+        youtube: "https://youtu.be/d63j_lkc3oU"
+    },
+    
+    {
+        level: 8,
+        sequence: 1,
+        title: "Little Cherries",
+        image: "images/08-little-cherries.pdf",
+        youtube: "https://youtu.be/vAUFSxfhrl4"
+    },
+    {
+        level: 8,
+        sequence: 2,
+        title: "Bluebells of Scotland",
+        image: "images/08-bluebells-of-scotland.pdf",
+        youtube: "https://youtu.be/dAM08smp5f4"
+    },
+    {
+        level: 8,
+        sequence: 3,
+        title: "Round the Mountain",
+        image: "images/08-shell-be-comin-round-the-mountain.pdf",
+        youtube: "https://youtu.be/9ARBJPZertA"
+    },
+    {
+        level: 8,
+        sequence: 4,
+        title: "The Ash Grove",
+        image: "images/08-the-ash-grove.pdf",
+        youtube: "https://youtu.be/OVZyjcuqq3g"
+
+    },
+    {
+        level: 8,
+        sequence: 5,
+        title: "The Old Gray Mare",
+        image: "images/08-old-gray-mare.pdf",
+        youtube: "https://youtu.be/q9K35TyL63U"
+    },
+    {
+        level: 8,
+        sequence: 6,
+        title: "Brahms' Lullaby",
+        image: "images/08-brahms-lullaby.pdf",
+        youtube: "https://youtu.be/c8inDbfO6IA"
+    },
+    {
+        level: 8,
+        sequence: 7,
+        title: "Ma Come Balli Bene Bella Bimba",
+        image: "images/08-ma-come-balli-bene.pdf",
+        youtube: "https://youtu.be/2GvH54kJrbw"
+    },
+    {
+        level: 8,
+        sequence: 8,
+        title: "Sailing",
+        image: "images/08-sailing.pdf",
+        youtube: "https://youtu.be/8Qo0LErUoWE"
+    },
+    {
+        level: 8,
+        sequence: 9,
+        title: "Good Morning",
+        image: "images/08-good-morning.pdf",
+        youtube: "https://youtu.be/_tUbCxGZ2Jg"
+
+    },
+    {
+        level: 8,
+        sequence: 10,
+        title: "The Barn Cat",
+        image: "images/08-the-barn-cat.pdf",
+        youtube: "https://youtu.be/YB3HSnDZWyk"
+
+    },
+    {
+        level: 8,
+        sequence: 11,
+        title: "The Star-Spangled Banner",
+        image: "images/08-the-star-spangled-banner.pdf",
+        youtube: "https://youtu.be/qWYggcXi6A0"
+    },
+    {
+        level: 9,
+        sequence: 1,
+        title: "Vive la Compagnie",
+        image: "images/09-vive-la-compagnie.pdf",
+        youtube:"https://youtu.be/gQYBOsZxTdE"
+    },   
+    {
+        level: 9,
+        sequence: 2,
+        title: "Over the River",
+        image: "images/09-over-the-river.pdf",
+        youtube:"https://youtu.be/PmaGKf1QeV4"
+    },   
+    {
+        level: 9,
+        sequence: 3,
+        title: "Johnny Comes Marching Home",
+        image: "images/09-when-johnny-comes-marching-home.pdf",
+        youtube: "https://youtu.be/tWs5bkQBpcs"
+    },   
+    {
+        level: 9,
+        sequence: 4,
+        title: "Jingle Bells",
+        image: "images/09-jingle-bells.pdf",
+        youtube:"https://youtu.be/91YjC0U-ioI"
+    },   
+    {
+        level: 9,
+        sequence: 5,
+        title: "Rondo Alla Turca",
+        image: "images/09-rondo-alla-turca.pdf",
+        youtube: "https://youtu.be/UPruEGCoxJM"
+
+    },   
+    {
+        level: 9,
+        sequence: 6,
+        title: "Morning Song",
+        image: "images/09-morning-song.pdf",
+        youtube: "https://youtu.be/eWX8_BCoLo4"
+    },   
+    {
+        level: 9,
+        sequence: 7,
+        title: "Canon",
+        image: "images/09-canon.pdf",
+        youtube: "https://youtu.be/F6fyvRlmbek"
+    },   
+    {
+        level: 9,
+        sequence: 8,
+        title: "Sugar Plum Fairy",
+        image: "images/09-dance-of-the-sugar-plum-fairy.pdf",
+        youtube: "https://youtu.be/OjY8-gxx0UA"
+    },   
+    {
+        level: 9,
+        sequence: 9,
+        title: "Rondo Alla Turca",
+        image: "images/09-rondo-alla-turca.pdf",
+        youtube: "https://youtu.be/UPruEGCoxJM"
+    },   
+    {
+        level: 9,
+        sequence: 10,
+        title: "Beethoven's 5th",
+        image: "images/09-beethovens-fifth.pdf",
+        youtube: "https://youtu.be/vvbYP2-MqTU"
+    },   
+    {
+        level: 9,
+        sequence: 11,
+        title: "Moonlight Sonata",
+        image: "images/09-moonlight-sonata.pdf",
+        youtube: "https://youtu.be/cQrLkO9bA1A"
+    },   
+    {
+        level: 9,
+        sequence: 12,
+        title: "The Entertainer",
+        image: "images/09-the-entertainer.pdf",
+        youtube: "https://youtu.be/F-AJMJty0oA"
+    },   
+    
+]
---
+++ scripts/userProfile.js
@@ -0,0 +1,11 @@
+export let userProfile = {
+    firstName: "Cameron",
+    lastName: "Doe",
+    nick: "Cam",
+    instructor: "",
+    handicap: 1,
+    level: 8,
+    pendingSongs: [],
+    failedSongs: [],
+    completedSongs: []
+}
