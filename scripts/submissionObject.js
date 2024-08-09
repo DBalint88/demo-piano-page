fda5158b6330ac81c17ab81f7b55f73ea77f4a9e
DBalint88 <dbalint88@gmail.com>
2024-08-09 13:35:20 -0400

    Built basic submission class

---
+++ scripts/submissionObject.js
@@ -0,0 +1,15 @@
+export let Submission = {
+    userID: null,
+    firstName: "",
+    lastName: "",
+    instructor: "",
+    pointValue: null,
+    resolved: false,
+    result: "",
+    songLevel: null,
+    songSeq: null,
+    songTitle: "",
+    songfbRef: "",
+    timeStamp: null,
+    week: null
+}
