e7bbcc0755b9ecb02124c34e21ea8054778c412b
DBalint88 <DBalint88@gmail.com>
2026-03-19 13:23:40 +0000

    fix week 84 nonsense

--- scripts/setWeek.js
+++ scripts/setWeek.js
@@ -1,7 +1,9 @@
 export function setWeek() {
-    const startDate = new Date('August 9, 2024')
-    const todaysDate = new Date()
-    const currentWeek = Math.ceil((todaysDate.getTime() - startDate.getTime()) / (1000 * 3600 * 24 * 7))
-    document.getElementById("weekid").innerText = currentWeek;
-    return currentWeek;
+  const startDate = new Date("March 8, 2026");
+  const todaysDate = new Date();
+  const currentWeek = Math.ceil(
+    (todaysDate.getTime() - startDate.getTime()) / (1000 * 3600 * 24 * 7),
+  );
+  document.getElementById("weekid").innerText = currentWeek;
+  return currentWeek;
 }
