76566df53edb2bc00d062d4bf0da4c459b81a753
DBalint88 <DBalint88@gmail.com>
2026-03-19 13:19:57 +0000

    Modify to show Levels 1-3

--- scripts/songData.js
+++ scripts/songData.js
@@ -1,279 +1,258 @@
 export const songData = [
-    {
-        level: 7,
-        sequence: 1,
-        title: "'Round the Mountain",
-        image: "images/07-round-the-mountain.pdf",
-        youtube: "https://youtu.be/T0kG5GiVvqQ",
-        fbRef: "701"
-    },
-    {
-        level: 7,
-        sequence: 2,
-        title: "In An English Country Garden",
-        image: "images/07-english-country-garden.pdf",
-        youtube: "https://youtu.be/E_ETiKC96uk",
-        fbRef: "702"
-    },
-    {
-        level: 7,
-        sequence: 3,
-        title: "Shoo Fly",
-        image: "images/07-shoo-fly.pdf",
-        youtube: "https://youtu.be/2Gh9HMHakio",
-        fbRef: "703"
-    },
-    {
-        level: 7,
-        sequence: 4,
-        title: "Molly Malone",
-        image: "images/07-molly-malone.pdf",
-        youtube: "https://youtu.be/8DZRbUwHnYc",
-        fbRef: "704"
-    },
-    {
-        level: 7,
-        sequence: 5,
-        title: "Korobushka",
-        image: "images/07-korobushka.pdf",
-        youtube: "https://youtu.be/t0hqlf3IxcU",
-        fbRef: "705"
-    },
-    {
-        level: 7,
-        sequence: 6,
-        title: "Eine Kleine Nachtmusik",
-        image: "images/07-eine-kleine.pdf",
-        youtube: "https://youtu.be/Jw7ub1ZP0Zs",
-        fbRef: "706"
-    },
-    {
-        level: 7,
-        sequence: 7,
-        title: "The Ants Go Marching",
-        image: "images/07-the-ants-go-marching.pdf",
-        youtube: "https://youtu.be/LtB_8rBhZk0",
-        fbRef: "707"
-    },
-    {
-        level: 7,
-        sequence: 8,
-        title: "Casey Jones",
-        image: "images/07-casey-jones.pdf",
-        youtube: "https://youtu.be/5JOpR2wZeNQ",
-        fbRef: "708"
-    },
-    {
-        level: 7,
-        sequence: 9,
-        title: "Home On the Range",
-        image: "images/07-home-on-the-range.pdf",
-        youtube: "https://youtu.be/Ku1LeHsDVVA",
-        fbRef: "709"
-    },
-    {
-        level: 7,
-        sequence: 10,
-        title: "Wild Mountain Thyme",
-        image: "images/07-wild-mountain-thyme.pdf",
-        youtube: "https://youtu.be/019KWL55QkM",
-        fbRef: "710"
-    },
-    {
-        level: 7,
-        sequence: 11,
-        title: "Troika",
-        image: "images/07-troika.pdf",
-        youtube: "https://youtu.be/d63j_lkc3oU",
-        fbRef: "711"
-    },
-    
-    {
-        level: 8,
-        sequence: 1,
-        title: "Little Cherries",
-        image: "images/08-little-cherries.pdf",
-        youtube: "https://youtu.be/vAUFSxfhrl4",
-        fbRef: "801"
-    },
-    {
-        level: 8,
-        sequence: 2,
-        title: "Bluebells of Scotland",
-        image: "images/08-bluebells-of-scotland.pdf",
-        youtube: "https://youtu.be/dAM08smp5f4",
-        fbRef: "802"
-    },
-    {
-        level: 8,
-        sequence: 3,
-        title: "Round the Mountain",
-        image: "images/08-shell-be-comin-round-the-mountain.pdf",
-        youtube: "https://youtu.be/9ARBJPZertA",
-        fbRef: "803"
-    },
-    {
-        level: 8,
-        sequence: 4,
-        title: "The Ash Grove",
-        image: "images/08-the-ash-grove.pdf",
-        youtube: "https://youtu.be/OVZyjcuqq3g",
-        fbRef: "804"
-
-    },
-    {
-        level: 8,
-        sequence: 5,
-        title: "The Old Gray Mare",
-        image: "images/08-old-gray-mare.pdf",
-        youtube: "https://youtu.be/q9K35TyL63U",
-        fbRef: "805"
-    },
-    {
-        level: 8,
-        sequence: 6,
-        title: "Brahms' Lullaby",
-        image: "images/08-brahms-lullaby.pdf",
-        youtube: "https://youtu.be/c8inDbfO6IA",
-        fbRef: "806"
-    },
-    {
-        level: 8,
-        sequence: 7,
-        title: "Ma Come Balli Bene Bella Bimba",
-        image: "images/08-ma-come-balli-bene.pdf",
-        youtube: "https://youtu.be/2GvH54kJrbw",
-        fbRef: "807"
-    },
-    {
-        level: 8,
-        sequence: 8,
-        title: "Sailing",
-        image: "images/08-sailing.pdf",
-        youtube: "https://youtu.be/8Qo0LErUoWE",
-        fbRef: "808"
-    },
-    {
-        level: 8,
-        sequence: 9,
-        title: "Good Morning",
-        image: "images/08-good-morning.pdf",
-        youtube: "https://youtu.be/_tUbCxGZ2Jg",
-        fbRef: "809"
-
-    },
-    {
-        level: 8,
-        sequence: 10,
-        title: "The Barn Cat",
-        image: "images/08-the-barn-cat.pdf",
-        youtube: "https://youtu.be/YB3HSnDZWyk",
-        fbRef: "810"
-
-    },
-    {
-        level: 8,
-        sequence: 11,
-        title: "The Star-Spangled Banner",
-        image: "images/08-the-star-spangled-banner.pdf",
-        youtube: "https://youtu.be/qWYggcXi6A0",
-        fbRef: "811"
-    },
-    {
-        level: 9,
-        sequence: 1,
-        title: "Vive la Compagnie",
-        image: "images/09-vive-la-compagnie.pdf",
-        youtube:"https://youtu.be/gQYBOsZxTdE",
-        fbRef: "901"
-    },   
-    {
-        level: 9,
-        sequence: 2,
-        title: "Over the River",
-        image: "images/09-over-the-river.pdf",
-        youtube:"https://youtu.be/PmaGKf1QeV4",
-        fbRef: "902"
-    },   
-    {
-        level: 9,
-        sequence: 3,
-        title: "Johnny Comes Marching Home",
-        image: "images/09-when-johnny-comes-marching-home.pdf",
-        youtube: "https://youtu.be/tWs5bkQBpcs",
-        fbRef: "903"
-    },   
-    {
-        level: 9,
-        sequence: 4,
-        title: "Jingle Bells",
-        image: "images/09-jingle-bells.pdf",
-        youtube:"https://youtu.be/91YjC0U-ioI",
-        fbRef: "904"
-    },   
-    {
-        level: 9,
-        sequence: 5,
-        title: "Rondo Alla Turca",
-        image: "images/09-rondo-alla-turca.pdf",
-        youtube: "https://youtu.be/UPruEGCoxJM",
-        fbRef: "905"
-    },   
-    {
-        level: 9,
-        sequence: 6,
-        title: "Morning Song",
-        image: "images/09-morning-song.pdf",
-        youtube: "https://youtu.be/eWX8_BCoLo4",
-        fbRef: "906"
-    },   
-    {
-        level: 9,
-        sequence: 7,
-        title: "Canon",
-        image: "images/09-canon.pdf",
-        youtube: "https://youtu.be/F6fyvRlmbek",
-        fbRef: "907"
-    },   
-    {
-        level: 9,
-        sequence: 8,
-        title: "Sugar Plum Fairy",
-        image: "images/09-dance-of-the-sugar-plum-fairy.pdf",
-        youtube: "https://youtu.be/OjY8-gxx0UA",
-        fbRef: "908"
-    },   
-    {
-        level: 9,
-        sequence: 9,
-        title: "Rondo Alla Turca",
-        image: "images/09-rondo-alla-turca.pdf",
-        youtube: "https://youtu.be/UPruEGCoxJM",
-        fbRef: "909"
-    },   
-    {
-        level: 9,
-        sequence: 10,
-        title: "Beethoven's 5th",
-        image: "images/09-beethovens-fifth.pdf",
-        youtube: "https://youtu.be/vvbYP2-MqTU",
-        fbRef: "910"
-    },   
-    {
-        level: 9,
-        sequence: 11,
-        title: "Moonlight Sonata",
-        image: "images/09-moonlight-sonata.pdf",
-        youtube: "https://youtu.be/cQrLkO9bA1A",
-        fbRef: "911"
-    },   
-    {
-        level: 9,
-        sequence: 12,
-        title: "The Entertainer",
-        image: "images/09-the-entertainer.pdf",
-        youtube: "https://youtu.be/F-AJMJty0oA",
-        fbRef: "912"
-    },   
-    
-]
+  {
+    level: 1,
+    sequence: 1,
+    title: "Hot Cross Buns",
+    image: "images/01-hot-cross-buns.pdf",
+    youtube: "https://youtu.be/3xDuF9zwbGU",
+    fbRef: "101",
+  },
+  {
+    level: 1,
+    sequence: 2,
+    title: "Mary Had A Little Lamb",
+    image: "images/01-mary-had-a-little-lamb.pdf",
+    youtube: "https://youtu.be/grDcfEkEO6I",
+    fbRef: "102",
+  },
+  {
+    level: 1,
+    sequence: 3,
+    title: "Au Claire de la Lune",
+    image: "images/01-au-claire-de-la-lune.pdf",
+    youtube: "https://youtu.be/1wXyVQZb1vU",
+    fbRef: "103",
+  },
+  {
+    level: 1,
+    sequence: 4,
+    title: "Jingle Bells",
+    image: "images/01-jingle-bells.pdf",
+    youtube: "https://youtu.be/heYQZ1XjY_M",
+    fbRef: "104",
+  },
+  {
+    level: 1,
+    sequence: 5,
+    title: "Rain, Rain, Go Away",
+    image: "images/01-rain-rain.pdf",
+    youtube: "https://youtu.be/bUSKOnnEkRI",
+    fbRef: "105",
+  },
+  {
+    level: 1,
+    sequence: 6,
+    title: "A Tisket, A Tasket",
+    image: "images/01-tisket-tasket",
+    youtube: "https://youtu.be/u3ggzUnmwoI",
+    fbRef: "106",
+  },
+  {
+    level: 1,
+    sequence: 7,
+    title: "Ode to Joy",
+    image: "images/01-ode-to-joy",
+    youtube: "https://youtu.be/Ecx6-i9Uo98",
+    fbRef: "107",
+  },
+  {
+    level: 1,
+    sequence: 8,
+    title: "Sweetly Sings the Donkey",
+    image: "images/01-sweetly-sings-the-donkey.pdf",
+    youtube: "https://youtu.be/4u8x60n98e0",
+    fbRef: "108",
+  },
+  {
+    level: 1,
+    sequence: 9,
+    title: "Go Tell Aunt Rhodie",
+    image: "images/01-go-tell-aunt-rhodie.pdf",
+    youtube: "https://youtu.be/ZgaeiGv81XA",
+    fbRef: "109",
+  },
+  {
+    level: 1,
+    sequence: 10,
+    title: "Banks of the Ohio",
+    image: "images/01-banks-of-the-ohio",
+    youtube: "https://youtu.be/rvptLO7993g",
+    fbRef: "110",
+  },
+  {
+    level: 2,
+    sequence: 1,
+    title: "Pop! Goes the Weasel",
+    image: "images/02-pop-goes-the-weasel.pdf",
+    youtube: "https://youtu.be/TiEeIBFmDT4",
+    fbRef: "201",
+  },
+  {
+    level: 2,
+    sequence: 2,
+    title: "Row, Row, Row Your Boat",
+    image: "images/02-row-row-row-your-boat.pdf",
+    youtube: "https://youtu.be/devbe8wYHPE",
+    fbRef: "202",
+  },
+  {
+    level: 2,
+    sequence: 3,
+    title: "Camptown Races",
+    image: "images/02-camptown-races.pdf",
+    youtube: "https://youtu.be/33a5mKYnPtQ",
+    fbRef: "203",
+  },
+  {
+    level: 2,
+    sequence: 4,
+    title: "Swan Lake",
+    image: "images/02-swan-lake.pdf",
+    youtube: "https://youtu.be/_x9tbgtcyms",
+    fbRef: "204",
+  },
+  {
+    level: 2,
+    sequence: 5,
+    title: "Auld Lang Syne",
+    image: "images/02-auld-lang-syne.pdf",
+    youtube: "https://youtu.be/w70ro945CgI",
+    fbRef: "205",
+  },
+  {
+    level: 2,
+    sequence: 6,
+    title: "Bridal March",
+    image: "images/02-bridal-march.pdf",
+    youtube: "https://youtu.be/FinbnEIVU4U",
+    fbRef: "206",
+  },
+  {
+    level: 2,
+    sequence: 7,
+    title: "London Bridge",
+    image: "images/02-london-bridge.pdf",
+    youtube: "https://youtu.be/ljUeJVxOWTY",
+    fbRef: "207",
+  },
+  {
+    level: 2,
+    sequence: 8,
+    title: "Five Fat Turkeys",
+    image: "images/08-five-fat-turkeys.pdf",
+    youtube: "https://youtu.be/9BYO15fcPiQ",
+    fbRef: "208",
+  },
+  {
+    level: 2,
+    sequence: 9,
+    title: "Yankee Doodle",
+    image: "images/09-yankee-doodle.pdf",
+    youtube: "https://youtu.be/dxkZ-2FkLLw",
+    fbRef: "209",
+  },
+  {
+    level: 2,
+    sequence: 10,
+    title: "Old MacDonald",
+    image: "images/02-old-macdonald.pdf",
+    youtube: "https://youtu.be/zUWiinHcVd8",
+    fbRef: "210",
+  },
+  {
+    level: 2,
+    sequence: 11,
+    title: "Twinkle, Twinkle, Little Star",
+    image: "images/02-twinkle-twinkle.pdf",
+    youtube: "https://youtu.be/5z5ESuEmG-k",
+    fbRef: "211",
+  },
+  {
+    level: 3,
+    sequence: 1,
+    title: "The Baby Bumble Bee",
+    image: "images/03-baby-bumble-bee.pdf",
+    youtube: "https://youtu.be/d1n9IAF2Img",
+    fbRef: "301",
+  },
+  {
+    level: 3,
+    sequence: 2,
+    title: "Happy Birthday",
+    image: "images/03-happy-birthday.pdf",
+    youtube: "https://youtu.be/3cRVwanUKqs",
+    fbRef: "302",
+  },
+  {
+    level: 3,
+    sequence: 3,
+    title: "BINGO",
+    image: "images/03-bingo.pdf",
+    youtube: "https://youtu.be/5uQ-rwRyE_E",
+    fbRef: "303",
+  },
+  {
+    level: 3,
+    sequence: 4,
+    title: "Ode to Joy (LH only)",
+    image: "images/03-ode-to-joy-lefty.pdf",
+    youtube: "https://youtu.be/Y40jIlPNS-E",
+    fbRef: "304",
+  },
+  {
+    level: 3,
+    sequence: 5,
+    title: "Aloutte",
+    image: "images/03-alouette.pdf",
+    youtube: "https://youtu.be/9pwv-b_SpNU",
+    fbRef: "305",
+  },
+  {
+    level: 3,
+    sequence: 6,
+    title: "Three Jolly Fishermen",
+    image: "images/03-jolly-fisherman.pdf",
+    youtube: "https://youtu.be/oekfdQMZZ-Y",
+    fbRef: "306",
+  },
+  {
+    level: 3,
+    sequence: 7,
+    title: "Row, Row, Row Your Boat",
+    image: "images/03-row-row-row-your-boat.pdf",
+    youtube: "https://youtu.be/0Go_0qoeMC8",
+    fbRef: "307",
+  },
+  {
+    level: 3,
+    sequence: 8,
+    title: "Turkey in the Straw",
+    image: "images/03-turkey-in-the-straw.pdf",
+    youtube: "https://youtu.be/VBDS8WRmDMI",
+    fbRef: "308",
+  },
+  {
+    level: 3,
+    sequence: 9,
+    title: "Banks of the Ohio",
+    image: "images/03-banks-of-the-ohio.pdf",
+    youtube: "https://youtu.be/hBefLxKf5To",
+    fbRef: "309",
+  },
+  {
+    level: 3,
+    sequence: 10,
+    title: "Ode to Joy",
+    image: "images/03-ode-to-joy.pdf",
+    youtube: "https://youtu.be/oqT5S3ZpqFI",
+    fbRef: "310",
+  },
+  {
+    level: 3,
+    sequence: 11,
+    title: "Jingle Bells",
+    image: "images/03-jingle-bells.pdf",
+    youtube: "https://youtu.be/mBPV8r9r1Ng",
+    fbRef: "311",
+  },
+];
