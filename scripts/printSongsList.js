a5e33d68272a348a1dd9667e61f4f86adc033a7a
DBalint88 <DBalint88@gmail.com>
2026-03-19 13:32:27 +0000

    adjust printSong logic to print levels 1-3, not 7-9

--- scripts/printSongsList.js
+++ scripts/printSongsList.js
@@ -5,31 +5,30 @@ import { clearData } from "./userInterface.js";
 
 export function getSongs(level, songData) {
   let userSongs = [];
-  for (let i=0; i < level; i++) {
+  for (let i = 0; i < level; i++) {
     let levelList = [];
-      
+
     for (let song of songData) {
-      if (song.level == (i+1)) {
+      if (song.level == i + 1) {
         levelList.push(song);
       }
     }
-    userSongs.push(levelList)
-    
+    userSongs.push(levelList);
   }
   return userSongs;
 }
 
-  //   window.window['level' + i] = []
-  //   let q = query(songsRef, where("level", "==", i), orderBy("sequence"))
-  //   await getDocs(q)
-  //   .then((snapshot) => {
-  //     snapshot.docs.forEach((doc) => {
-  //       window['level' + i].push({ ...doc.data(), id: doc.id })
-  //     })
-  //     songs.push(window['level' + i])
-  //     console.log("getSongs says: ", window['level' + i])
-  //   })
-  // }
+//   window.window['level' + i] = []
+//   let q = query(songsRef, where("level", "==", i), orderBy("sequence"))
+//   await getDocs(q)
+//   .then((snapshot) => {
+//     snapshot.docs.forEach((doc) => {
+//       window['level' + i].push({ ...doc.data(), id: doc.id })
+//     })
+//     songs.push(window['level' + i])
+//     console.log("getSongs says: ", window['level' + i])
+//   })
+// }
 //   updateUserLevel()
 //   if (x == userLevel) {
 //     updateSongListLive()
@@ -38,111 +37,117 @@ export function getSongs(level, songData) {
 //   printSongs()
 // }
 
+export function printSongsList(
+  comps,
+  displayState,
+  userProfile,
+  callSongList,
+  loadSong,
+) {
+  let levelList = document.createElement("div");
+  levelList.setAttribute("id", "level-list");
+  let levelUl = document.createElement("ul");
+  levelUl.setAttribute("id", "level-ul");
+  comps.navListWrapper.appendChild(levelList);
+  levelList.appendChild(levelUl);
 
-export function printSongsList (comps, displayState, userProfile, callSongList, loadSong) {
-    let levelList = document.createElement('div')
-    levelList.setAttribute('id', 'level-list')
-    let levelUl = document.createElement('ul')
-    levelUl.setAttribute('id', 'level-ul')
-    comps.navListWrapper.appendChild(levelList)
-    levelList.appendChild(levelUl)
-  
-    for (let i=7; i <= userProfile.viewableSongs.length; i++) {
-  
-      // Print the level list
-      let levelButton = document.createElement('li')
-      levelButton.classList.add("level-button")
-      levelButton.setAttribute("id", i)
-      levelButton.textContent = 'Level ' + i
-      levelUl.appendChild(levelButton)
-      levelButton.addEventListener('click', function(event) {
-        const e = event.target.id;
-        callSongList(e, displayState, levelList, comps)
+  for (let i = 1; i <= userProfile.viewableSongs.length; i++) {
+    // Print the level list
+    let levelButton = document.createElement("li");
+    levelButton.classList.add("level-button");
+    levelButton.setAttribute("id", i);
+    levelButton.textContent = "Level " + i;
+    levelUl.appendChild(levelButton);
+    levelButton.addEventListener("click", function (event) {
+      const e = event.target.id;
+      callSongList(e, displayState, levelList, comps);
+    });
+
+    let songsContainer = document.createElement("div");
+    songsContainer.classList.add("song-list");
+    songsContainer.setAttribute("id", "level-" + i);
+
+    let levelHeader = document.createElement("h2");
+    levelHeader.textContent = "Level " + i;
+
+    let levelValueHeader = document.createElement("h4");
+    levelValueHeader.textContent =
+      "(" + determineSongValue(i, userProfile.handicap) + " points each)";
+
+    let levelOl = document.createElement("ol");
+
+    comps.navListWrapper.appendChild(songsContainer);
+    songsContainer.appendChild(levelHeader);
+    songsContainer.appendChild(levelValueHeader);
+    songsContainer.appendChild(levelOl);
+
+    // Print the song buttons
+
+    for (let j = 0; j < userProfile.viewableSongs[i - 1].length; j++) {
+      let song = document.createElement("li");
+      song.classList.add("song-button");
+      let songSrc = userProfile.viewableSongs[i - 1][j];
+      song.setAttribute("data-level", i);
+      song.setAttribute("data-seq", j + 1);
+      song.setAttribute("data-pdf", songSrc.image);
+      song.setAttribute("data-video", songSrc.youtube);
+      song.setAttribute("data-fbref", songSrc.fbRef);
+      song.textContent = songSrc.title;
+
+      // Print the status icons
+
+      let statusIcon = document.createElement("img");
+      statusIcon.setAttribute("src", "images/default-status-icon.png");
+      statusIcon.setAttribute("id", songSrc.fbRef);
+      statusIcon.classList.add("status-icon");
+
+      levelOl.appendChild(song);
+      song.appendChild(statusIcon);
+      song.addEventListener("click", function (event) {
+        const e = event.target;
+        loadSong(e, displayState, comps, userProfile);
       });
-  
-      let songsContainer = document.createElement("div")
-      songsContainer.classList.add('song-list')
-      songsContainer.setAttribute("id", 'level-'+i)
-  
-      let levelHeader = document.createElement("h2")
-      levelHeader.textContent = 'Level ' + i
-  
-      let levelValueHeader = document.createElement('h4')
-      levelValueHeader.textContent = '(' + determineSongValue(i, userProfile.handicap) + ' points each)'
-  
-      let levelOl = document.createElement("ol")
-  
-      comps.navListWrapper.appendChild(songsContainer)
-      songsContainer.appendChild(levelHeader)
-      songsContainer.appendChild(levelValueHeader)
-      songsContainer.appendChild(levelOl)
-  
-  
-      // Print the song buttons
-      
-      for(let j=0; j<userProfile.viewableSongs[i-1].length; j++) {
-        let song = document.createElement("li");
-        song.classList.add("song-button")
-        let songSrc = userProfile.viewableSongs[i-1][j]
-        song.setAttribute("data-level", i)
-        song.setAttribute("data-seq", (j+1))
-        song.setAttribute("data-pdf", songSrc.image)
-        song.setAttribute("data-video", songSrc.youtube)
-        song.setAttribute("data-fbref", songSrc.fbRef)
-        song.textContent = songSrc.title
-  
-        // Print the status icons
-        
-        let statusIcon = document.createElement('img')
-        statusIcon.setAttribute('src', 'images/default-status-icon.png')
-        statusIcon.setAttribute('id', songSrc.fbRef)
-        statusIcon.classList.add('status-icon')
-  
-        levelOl.appendChild(song)
-        song.appendChild(statusIcon)
-        song.addEventListener('click', function(event) {
-          const e = event.target;
-          loadSong(e, displayState, comps, userProfile);
-        });
-  
-      }
+    }
+  }
+}
+
+export function checkUserProgress(comps, userProfile, songData, displayState) {
+  if (displayState.currentSongLevel > userProfile.level) {
+    return false;
+  }
+
+  let allCurrentLevelSongs = [];
+  console.log(userProfile.viewableSongs);
+
+  for (const song of userProfile.viewableSongs[userProfile.level - 1]) {
+    if (song.level == userProfile.level) {
+      allCurrentLevelSongs.push(song.fbRef);
     }
   }
 
-  export function checkUserProgress(comps, userProfile, songData, displayState) {
+  console.log("allCurrentLevelSongs = " + allCurrentLevelSongs);
 
-    if (displayState.currentSongLevel > userProfile.level) {
-      return false;
-    }
+  let completedSongsPlusPendingSongs = userProfile.completedSongs.concat(
+    userProfile.pendingSongs,
+  );
+  console.log(
+    "completedSongsPlusPendingSongs = " + completedSongsPlusPendingSongs,
+  );
 
-    let allCurrentLevelSongs = []
-    console.log(userProfile.viewableSongs)
-    
-    for (const song of userProfile.viewableSongs[userProfile.level - 1])  {
-        if (song.level == userProfile.level) {
-          allCurrentLevelSongs.push(song.fbRef)
-        }
-    }
-  
-    console.log("allCurrentLevelSongs = " + allCurrentLevelSongs);
-  
-    let completedSongsPlusPendingSongs = userProfile.completedSongs.concat(userProfile.pendingSongs)
-    console.log("completedSongsPlusPendingSongs = " +completedSongsPlusPendingSongs)
-  
-    let checker = (arr, target) => target.every(v => arr.includes(v));
-  
-    // if all of the songs at the user's current level are COMPLETE, user's level is advanced and the song list re-loaded.
-    if (checker(userProfile.completedSongs, allCurrentLevelSongs)) {
-      userProfile.level += 1;
-      clearData(comps, userProfile)
-      userProfile.viewableSongs = getSongs(userProfile.level, songData)
-      return true;
+  let checker = (arr, target) => target.every((v) => arr.includes(v));
+
+  // if all of the songs at the user's current level are COMPLETE, user's level is advanced and the song list re-loaded.
+  if (checker(userProfile.completedSongs, allCurrentLevelSongs)) {
+    userProfile.level += 1;
+    clearData(comps, userProfile);
+    userProfile.viewableSongs = getSongs(userProfile.level, songData);
+    return true;
     // if all of the songs at the user's current level are either complete or pending, and at least some are pending, user should be granted access to the next level, without advancing their profile level.
-    } else if (checker(completedSongsPlusPendingSongs, allCurrentLevelSongs)) {
-      clearData(comps, userProfile)
-      userProfile.viewableSongs = getSongs(userProfile.level + 1, songData)
-      return true;
-    } else {
-      return false;
-    }
+  } else if (checker(completedSongsPlusPendingSongs, allCurrentLevelSongs)) {
+    clearData(comps, userProfile);
+    userProfile.viewableSongs = getSongs(userProfile.level + 1, songData);
+    return true;
+  } else {
+    return false;
   }
+}
--- scripts/userInterface.js
+++ scripts/userInterface.js
@@ -1,200 +1,243 @@
-import { createSubmission, countCurrentSongAttempts, submitSong, retractSubmission } from './createSubmission.js';
-import { updateStatusLights } from './updateStatusLights.js';
+import {
+  createSubmission,
+  countCurrentSongAttempts,
+  submitSong,
+  retractSubmission,
+} from "./createSubmission.js";
+import { updateStatusLights } from "./updateStatusLights.js";
 
-export function activateUI(comps, displayState, userProfile, submissionBank, songData, callSongList, loadSong, printSongsList) {
-    
-    comps.logoutButton.addEventListener('click', () => {
-      comps.splashGreeting.innerText = "Please log in to your fake demo account:"
+export function activateUI(
+  comps,
+  displayState,
+  userProfile,
+  submissionBank,
+  songData,
+  callSongList,
+  loadSong,
+  printSongsList,
+) {
+  comps.logoutButton.addEventListener("click", () => {
+    comps.splashGreeting.innerText = "Please log in to your fake demo account:";
 
-      comps.logoutButton.style.display = 'none'
-      comps.loginButton.style.display = 'flex'
+    comps.logoutButton.style.display = "none";
+    comps.loginButton.style.display = "flex";
 
-      clearData(comps, userProfile)
-
-    })
-    comps.backButton.addEventListener("click", function() {
-      hideSongList(displayState, comps)
-      });
-    comps.homeButton.addEventListener("click", function() {
-      goHome(comps, displayState, userProfile)
-    });
-    comps.submitButton.addEventListener("click", function() {
-      submitSong(comps, displayState, userProfile, submissionBank, songData, callSongList, loadSong, printSongsList);
-    });
-    comps.logoutButton.addEventListener("click", () => {
-    });
-    window.addEventListener('resize', () => {
-      adjustListPosition(comps);
-    });
-  
+    clearData(comps, userProfile);
+  });
+  comps.backButton.addEventListener("click", function () {
+    hideSongList(displayState, comps);
+  });
+  comps.homeButton.addEventListener("click", function () {
+    goHome(comps, displayState, userProfile);
+  });
+  comps.submitButton.addEventListener("click", function () {
+    submitSong(
+      comps,
+      displayState,
+      userProfile,
+      submissionBank,
+      songData,
+      callSongList,
+      loadSong,
+      printSongsList,
+    );
+  });
+  comps.logoutButton.addEventListener("click", () => {});
+  window.addEventListener("resize", () => {
+    adjustListPosition(comps);
+  });
 }
 
-
-export  function callSongList(e, displayState, levelList, comps) {
-    displayState.currentActiveLevel = e;
-    let currentActiveSongList = document.getElementById("level-" + e);
-    levelList.classList.add("inactive-level-list");
-    currentActiveSongList.classList.add('song-list-loading')
-    setTimeout(function(){
-      currentActiveSongList.classList.add("active-song-list")
-      currentActiveSongList.classList.remove('song-list-loading')
-      comps.backButton.classList.add("back-button-active") 
-      adjustListPosition(comps)
-    }, 1) 
+export function callSongList(e, displayState, levelList, comps) {
+  displayState.currentActiveLevel = e;
+  let currentActiveSongList = document.getElementById("level-" + e);
+  levelList.classList.add("inactive-level-list");
+  currentActiveSongList.classList.add("song-list-loading");
+  setTimeout(function () {
+    currentActiveSongList.classList.add("active-song-list");
+    currentActiveSongList.classList.remove("song-list-loading");
+    comps.backButton.classList.add("back-button-active");
+    adjustListPosition(comps);
+  }, 1);
 }
 
 export function hideSongList(displayState, comps) {
-    let currentActiveSongList = document.getElementById("level-" + displayState.currentActiveLevel)
-    // song-list-loading needs to be added FIRST THING.  That allows the list to display.
-    // Immediately, active-song-list must be removed.  That creates the transition.
-    // 250ms later, song-list-loading needs to be removed to reduce the navlistwrapper size.
-    currentActiveSongList.classList.add('song-list-loading')
-    currentActiveSongList.classList.remove("active-song-list")
-    currentActiveSongList.classList.remove("active-song-list-short-screen")
-    document.getElementById("level-list").classList.remove("inactive-level-list");
-    comps.backButton.classList.remove("back-button-active") 
-    setTimeout(function(){
-      currentActiveSongList.classList.remove('song-list-loading')
-      
-    }, 250) 
-    displayState.currentActiveLevel = ""
+  let currentActiveSongList = document.getElementById(
+    "level-" + displayState.currentActiveLevel,
+  );
+  // song-list-loading needs to be added FIRST THING.  That allows the list to display.
+  // Immediately, active-song-list must be removed.  That creates the transition.
+  // 250ms later, song-list-loading needs to be removed to reduce the navlistwrapper size.
+  currentActiveSongList.classList.add("song-list-loading");
+  currentActiveSongList.classList.remove("active-song-list");
+  currentActiveSongList.classList.remove("active-song-list-short-screen");
+  document.getElementById("level-list").classList.remove("inactive-level-list");
+  comps.backButton.classList.remove("back-button-active");
+  setTimeout(function () {
+    currentActiveSongList.classList.remove("song-list-loading");
+  }, 250);
+  displayState.currentActiveLevel = "";
 }
-  
+
 export function loadSong(e, displayState, comps, userProfile) {
-    comps.splash.style.display = "none";
-    comps.iframe.style.width = "100%";
-    comps.iframe.style.height = "100%";
-    comps.iframe.src = e.dataset.pdf + "#zoom=118&navpanes=0&pagemode=none";
-    comps.videoLink.href = e.dataset.video;
-    comps.pdfLink.href = e.dataset.pdf +"#zoom=83";
-    displayState.currentSongFbref = e.dataset.fbref
-    displayState.currentSongTitle = e.textContent
-    displayState.currentSongLevel = parseInt(e.dataset.level)
-    displayState.currentSongSeq = parseInt(e.dataset.seq)
-    displayState.currentSongValue = determineSongValue(displayState.currentSongLevel, userProfile.handicap)
-    updateButtons(comps, displayState, userProfile); 
+  comps.splash.style.display = "none";
+  comps.iframe.style.width = "100%";
+  comps.iframe.style.height = "100%";
+  comps.iframe.src = e.dataset.pdf + "#zoom=118&navpanes=0&pagemode=none";
+  comps.videoLink.href = e.dataset.video;
+  comps.pdfLink.href = e.dataset.pdf + "#zoom=83";
+  displayState.currentSongFbref = e.dataset.fbref;
+  displayState.currentSongTitle = e.textContent;
+  displayState.currentSongLevel = parseInt(e.dataset.level);
+  displayState.currentSongSeq = parseInt(e.dataset.seq);
+  displayState.currentSongValue = determineSongValue(
+    displayState.currentSongLevel,
+    userProfile.handicap,
+  );
+  updateButtons(comps, displayState, userProfile);
 }
-  
+
 export function adjustListPosition(comps) {
-    let wrapperHeight = comps.navListWrapper.clientHeight;
+  let wrapperHeight = comps.navListWrapper.clientHeight;
 
-    setTimeout(function(){
-        try {
-        let songList = document.getElementsByClassName('active-song-list')[0]
-        if (typeof songList == "undefined") {
-            songList = document.getElementsByClassName('active-song-list-short-screen')[0]
-        }
-        let songListHeight = songList.clientHeight;
+  setTimeout(function () {
+    try {
+      let songList = document.getElementsByClassName("active-song-list")[0];
+      if (typeof songList == "undefined") {
+        songList = document.getElementsByClassName(
+          "active-song-list-short-screen",
+        )[0];
+      }
+      let songListHeight = songList.clientHeight;
 
-        if (wrapperHeight >= songListHeight) {
-            console.log('wrapperHeight (' + wrapperHeight + ') >= songListHeight (' + songListHeight + ')')
-            songList.classList.remove('active-song-list-short-screen')
-            songList.classList.add('active-song-list')
-        } else {
-            console.log('wrapperHeight (' + wrapperHeight + ') !>= songListHeight (' + songListHeight + ')')
-            songList.classList.remove('active-song-list')
-            songList.classList.add('active-song-list-short-screen')
-        }
-        } catch {
-        console.log("can't get the height of a div that don't exist.")
-        }
-    }, 1)  
+      if (wrapperHeight >= songListHeight) {
+        console.log(
+          "wrapperHeight (" +
+            wrapperHeight +
+            ") >= songListHeight (" +
+            songListHeight +
+            ")",
+        );
+        songList.classList.remove("active-song-list-short-screen");
+        songList.classList.add("active-song-list");
+      } else {
+        console.log(
+          "wrapperHeight (" +
+            wrapperHeight +
+            ") !>= songListHeight (" +
+            songListHeight +
+            ")",
+        );
+        songList.classList.remove("active-song-list");
+        songList.classList.add("active-song-list-short-screen");
+      }
+    } catch {
+      console.log("can't get the height of a div that don't exist.");
+    }
+  }, 1);
 }
-  
-
 
 export function goHome(comps, displayState, userProfile) {
-    comps.iframe.style.width = '0';
-    comps.iframe.style.height = '0';
-    comps.videoLink.href = ''
-    comps.pdfLink.href = ''
-    comps.splash.style.display = "block";
-    displayState.currentSongFbref = ''
-    displayState.currentSongTitle = ''
-    displayState.currentSongLevel = 0
-    displayState.currentSongValue = 0
-    displayState.currentSongAttempts = 0
-    updateButtons(comps, displayState, userProfile);
+  comps.iframe.style.width = "0";
+  comps.iframe.style.height = "0";
+  comps.videoLink.href = "";
+  comps.pdfLink.href = "";
+  comps.splash.style.display = "block";
+  displayState.currentSongFbref = "";
+  displayState.currentSongTitle = "";
+  displayState.currentSongLevel = 0;
+  displayState.currentSongValue = 0;
+  displayState.currentSongAttempts = 0;
+  updateButtons(comps, displayState, userProfile);
 }
 
 // CONTROL APPEARANCE OF YT, PDF, HOME, & SUBMIT BUTTONS
 export function updateButtons(comps, displayState, userProfile) {
-  console.log("updateButtons fired!")
-  if (displayState.currentSongFbref == '') {
-    comps.videoIcon.style.opacity = ".2"
-    comps.videoLink.style.pointerEvents="none"
-    comps.pdfIcon.style.opacity = ".2"
-    comps.pdfLink.style.pointerEvents="none"
-    comps.submitButton.style.opacity = ".2"
-    comps.submitButton.style.pointerEvents="none"
+  console.log("updateButtons fired!");
+  if (displayState.currentSongFbref == "") {
+    comps.videoIcon.style.opacity = ".2";
+    comps.videoLink.style.pointerEvents = "none";
+    comps.pdfIcon.style.opacity = ".2";
+    comps.pdfLink.style.pointerEvents = "none";
+    comps.submitButton.style.opacity = ".2";
+    comps.submitButton.style.pointerEvents = "none";
   } else {
-    comps.videoIcon.style.opacity = "1"
-    comps.videoLink.style.cursor = "pointer"
-    comps.videoLink.style.pointerEvents = "auto"
-    comps.pdfIcon.style.opacity = "1"
-    comps.pdfLink.style.cursor = "pointer"
-    comps.pdfLink.style.pointerEvents = "auto"
-    comps.submitButton.style.pointerEvents="auto"
+    comps.videoIcon.style.opacity = "1";
+    comps.videoLink.style.cursor = "pointer";
+    comps.videoLink.style.pointerEvents = "auto";
+    comps.pdfIcon.style.opacity = "1";
+    comps.pdfLink.style.cursor = "pointer";
+    comps.pdfLink.style.pointerEvents = "auto";
+    comps.submitButton.style.pointerEvents = "auto";
     if (userProfile.completedSongs.includes(displayState.currentSongFbref)) {
-      console.log("completedSongs includes currentSongFbref")
-      comps.submitButton.src = "images/upload-icon.png"
-      comps.submitButton.style.opacity = ".2"
-      comps.submitButton.style.cursor = "default"
-    } else if (userProfile.pendingSongs.includes(displayState.currentSongFbref)) {
-      console.log("pendingSongs includes currentSongFbref")
-      comps.submitButton.src = "images/undo-icon.png"
-      comps.submitButton.style.opacity = "1"
-      comps.submitButton.style.cursor = "pointer"
+      console.log("completedSongs includes currentSongFbref");
+      comps.submitButton.src = "images/upload-icon.png";
+      comps.submitButton.style.opacity = ".2";
+      comps.submitButton.style.cursor = "default";
+    } else if (
+      userProfile.pendingSongs.includes(displayState.currentSongFbref)
+    ) {
+      console.log("pendingSongs includes currentSongFbref");
+      comps.submitButton.src = "images/undo-icon.png";
+      comps.submitButton.style.opacity = "1";
+      comps.submitButton.style.cursor = "pointer";
     } else {
-      console.log("failedSongs includes currentSongFbref, or no arrays do.")
-      comps.submitButton.src = "images/upload-icon.png"
-      comps.submitButton.style.opacity = "1"
-      comps.submitButton.style.cursor = "pointer"
+      console.log("failedSongs includes currentSongFbref, or no arrays do.");
+      comps.submitButton.src = "images/upload-icon.png";
+      comps.submitButton.style.opacity = "1";
+      comps.submitButton.style.cursor = "pointer";
     }
   }
 }
 
 // UPDATE THE QUOTA DISPLAY
 export function updateQuotaDisplay(displayState, userProfile, submissionBank) {
-    let attempted = 0;
-    let earned = 0;
+  let attempted = 0;
+  let earned = 0;
 
-    for (const sub of submissionBank) {
-      if (sub.userID == userProfile.userID && sub.week == displayState.currentWeek && sub.result == "") {
-        attempted += sub.pointValue;
-      }
-      if (sub.userID == userProfile.userID && sub.week == displayState.currentWeek && sub.result == "pass") {
-        earned += sub.pointValue;
-      }
+  for (const sub of submissionBank) {
+    if (
+      sub.userID == userProfile.userID &&
+      sub.week == displayState.currentWeek &&
+      sub.result == ""
+    ) {
+      attempted += sub.pointValue;
     }
-
-    document.getElementById("points-attempted").innerText = attempted;
-    document.getElementById("points-earned").innerText = earned;
+    if (
+      sub.userID == userProfile.userID &&
+      sub.week == displayState.currentWeek &&
+      sub.result == "pass"
+    ) {
+      earned += sub.pointValue;
+    }
   }
 
+  document.getElementById("points-attempted").innerText = attempted;
+  document.getElementById("points-earned").innerText = earned;
+}
 
-  // CLEAR DISPLAY ON LOG OUT, OR TO RESET PAGE ON LEVEL CHANGES
+// CLEAR DISPLAY ON LOG OUT, OR TO RESET PAGE ON LEVEL CHANGES
 export function clearData(comps, userProfile) {
-    userProfile.viewableSongs = [];
-    comps.backButton.classList.remove("back-button-active")
-    while (comps.navListWrapper.firstChild) {
-      comps.navListWrapper.removeChild(comps.navListWrapper.firstChild)
-    }
-    // Quota display:
-    document.getElementById("points-attempted").innerText = "";
-    document.getElementById("points-earned").innerText = "";
+  userProfile.viewableSongs = [];
+  comps.backButton.classList.remove("back-button-active");
+  while (comps.navListWrapper.firstChild) {
+    comps.navListWrapper.removeChild(comps.navListWrapper.firstChild);
+  }
+  // Quota display:
+  document.getElementById("points-attempted").innerText = "";
+  document.getElementById("points-earned").innerText = "";
 }
 
 //  DETERMINE POINT VALUE OF CURRENT SONG TOWARDS WEEKLY QUOTA
 export function determineSongValue(songLevel, handicap) {
   switch (songLevel) {
-    case 7:
-      return 15 * handicap
-    case 8:
-      return 20 * handicap
-    case 9:
-      return 30 * handicap
+    case 1:
+      return 15 * handicap;
+    case 2:
+      return 20 * handicap;
+    case 3:
+      return 30 * handicap;
     default:
-      return 60
+      return 60;
   }
 }
